875f2b5acda96334640b7fc7a342078426d638ce
bujosa <davidbujosa@gmail.com>
2021-06-11 03:27:00 -0400

    Finished encodingBase64 and decodingBuffer

--- src/upload/helper/bg-image/encoding-and-decoding.ts
+++ src/upload/helper/bg-image/encoding-and-decoding.ts
@@ -1,12 +1,9 @@
-var b64string = /* whatever */;
-var buf = new Buffer(b64string, 'base64'); // Ta-da
+import { Base64 } from 'aws-sdk/clients/ecr';
 
-// encoding and decoding base64
+export const decodingBuffer = (buffer: Buffer): Base64 => {
+  return buffer.toString('base64');
+};
 
-'use strict';
-
-let data = 'stackabuse.com';
-let buff = new Buffer(data);
-let base64data = buff.toString('base64');
-
-console.log('"' + data + '" converted to Base64 is "' + base64data + '"');
+export const encodingBase64 = (data: Base64): Buffer => {
+  return Buffer.from(data, 'base64');
+};
